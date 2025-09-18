50f798a758b4402ed2a79857307ce42bb8ae29c9
parsapaktinat <parsapaktinat63@gmail.com>
2025-09-18 18:27:33 +0330

    Ship can now go left and right continuously.

--- alien_invasion.py
+++ alien_invasion.py
@@ -26,6 +26,7 @@ class AlienInvasion:
         while True:
             # Watch for keyboard and mouse events.
             self._check_events()
+            self.ship.update()
             self._update_screen()
             self.clock.tick(60)
 
@@ -36,11 +37,14 @@ class AlienInvasion:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    # Move the ship to the right.
-                    self.ship.rect.x += 1
-                elif event.key == pygame.K_left:
-                    # Move the ship to the left
-                    self.ship.rect.y -= 1
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
