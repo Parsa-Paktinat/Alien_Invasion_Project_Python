078c910b656abc4f389456f4c7c94889a4822c39
parsapaktinat <parsapaktinat63@gmail.com>
2025-09-19 17:15:32 +0330

    Bullet settings added to the Settings class

--- settings.py
+++ settings.py
@@ -7,4 +7,13 @@ class Settings:
         self.screen_width = 1000
         self.screen_height = 600
         self.bg_color = (150, 150, 150)
-        self.ship_speed = 3
+
+        # Ship settings
+        self.ship_speed = 2.5
+
+        # Bullet settings
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (50, 50, 50)
+        
