1fd57a0483f0adb89098fb9a486023c68da19f0b
parsapaktinat <parsapaktinat63@gmail.com>
2025-09-19 17:30:32 +0330

    Ship's speed decreased.

--- settings.py
+++ settings.py
@@ -9,11 +9,10 @@ class Settings:
         self.bg_color = (150, 150, 150)
 
         # Ship settings
-        self.ship_speed = 2.5
+        self.ship_speed = 1.5
 
         # Bullet settings
         self.bullet_speed = 2.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (50, 50, 50)
-        
