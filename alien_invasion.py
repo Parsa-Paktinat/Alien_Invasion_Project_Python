f0b82d4d1402e96c81632d636165a8597197e714
parsapaktinat <parsapaktinat63@gmail.com>
2025-09-21 17:54:58 +0330

    New functions for handling movement to the right and left and dropping down added.

--- alien_invasion.py
+++ alien_invasion.py
@@ -123,6 +123,19 @@ class AlienInvasion:
         """Update the positions of all aliens in the fleet."""
         self.aliens.update()
 
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.aliens_drop_speed
+        self.settings.fleet_direction *= -1
+
 
 if __name__ == '__main__':
     # Make a game instance and run the game.
