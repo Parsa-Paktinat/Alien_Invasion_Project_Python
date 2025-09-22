04516570435c89b584c790d4ba8c3b7c05eb8090
parsapaktinat <parsapaktinat63@gmail.com>
2025-09-22 13:25:12 +0330

    Method for drawing button added.

--- button.py
+++ button.py
@@ -25,4 +25,9 @@ class Button:
         """Turn msg into a rendered image and center text on the button."""
         self.msg_image = self.font.render(msg, True, self.text_color, self.button_color)
         self.msg_image_rect = self.msg_image.get_rect()
-        self.msg_message_rect.center = self.rect.center
+        self.msg_image_rect.center = self.rect.center
+
+    def draw_button(self):
+        """Draw blank button and then draw message."""
+        self.screen.fill(self.button_color, self.rect)
+        self.screen.blit(self.msg_image, self.msg_image_rect)
